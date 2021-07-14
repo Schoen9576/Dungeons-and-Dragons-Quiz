a6220ed9d0f0d73f19b23bd4362058a4549e085a
Axel Xero <schoen9576@gmail.com>
2021-07-13 21:21:52 -0500

    added more variables that will be needed

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
-var startButtonEl = document.querySelector("#start")
-var highScoreButtonEl = document.querySelector("#highScoreBtn") 
-var questionContainerEl = document.querySelector(".quiz-container")
-var questionTextEl = document.querySelector("#question")
-var answerTextEl = document.querySelector("#answers")
-var resultsEl = document.querySelector("#results")
-var timerEl = document.querySelector("#timer")
-var timeRemainingEl = document.querySelector("#timeRemaining")
+var startButtonEl = document.querySelector("#start");
+var highScoreButtonEl = document.querySelector("#highScoreBtn");
+var questionContainerEl = document.querySelector(".quiz-container");
+var questionTextEl = document.querySelector("#question");
+var answerTextEl = document.querySelector("#answers");
+var resultsEl = document.querySelector("#results");
+var timerEl = document.querySelector("#timer");
+var timeRemainingEl = document.querySelector("#timeRemaining");
 var timeRemaining = 60;
+var scoreSaveEl = document.querySelector("scoreSave");
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
+var score = 0;
+var highscore = [];
+var initials = [];
+
+
 
 startButtonEl.addEventListener('click', function() {
     startQuiz();
